b113347b390a93fafde2b8589300d4e68163d67d
robot <guillermuena@gmail.com>
2021-05-28 16:07:56 -0300

    Initial Commit

---
+++ scripts/panda_dir.py
@@ -0,0 +1,23 @@
+import rospy
+import time
+import panda_driver_motors
+from motors_panda.msg import Motors
+
+def callback(data):
+    rospy.loginfo("I heard LEFT: %f", data.data_l)
+    rospy.loginfo("I heard RIGHT: %f", data.data_r)
+    panda_driver_motors.move_dir_FW (data.data_l,data.data_r)
+    time.sleep(1)
+    panda_driver_motors.stop()
+    time.sleep(1)
+
+def listener():
+
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("chatter", Motors, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
