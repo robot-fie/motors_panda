4c28877ecacce24c82e16af9e2f026bb65ecdd14
robot <guillermuena@gmail.com>
2021-05-28 17:00:11 -0300

    Eliminado el stop post comando de velocidad

--- scripts/panda_dir.py
+++ scripts/panda_dir.py
@@ -7,9 +7,9 @@ def callback(data):
     rospy.loginfo("I heard LEFT: %f", data.data_l)
     rospy.loginfo("I heard RIGHT: %f", data.data_r)
     panda_driver_motors.move_dir_FW (data.data_l,data.data_r)
-    time.sleep(1)
-    panda_driver_motors.stop()
-    time.sleep(1)
+    #time.sleep(1)
+    #panda_driver_motors.stop()
+    #time.sleep(1)
 
 def listener():
 
