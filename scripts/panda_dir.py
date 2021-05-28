df0ef2164d730bbcb5142a0619b1b36553c26977
robot <guillermuena@gmail.com>
2021-05-28 17:11:27 -0300

    agregado Stop con mensaje 0.0,0.0

--- scripts/panda_dir.py
+++ scripts/panda_dir.py
@@ -6,7 +6,10 @@ from motors_panda.msg import Motors
 def callback(data):
     rospy.loginfo("I heard LEFT: %f", data.data_l)
     rospy.loginfo("I heard RIGHT: %f", data.data_r)
-    panda_driver_motors.move_dir_FW (data.data_l,data.data_r)
+    if (data.data_l == 0.0 and data.data_r == 0.0):
+        panda_driver_motors.stop()
+    else:
+        panda_driver_motors.move_dir_FW (data.data_l,data.data_r)
     #time.sleep(1)
     #panda_driver_motors.stop()
     #time.sleep(1)
