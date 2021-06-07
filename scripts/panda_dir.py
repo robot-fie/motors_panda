e34daa2db93824b048ccb1a32e77ba2cf0ecf430
robot <guillermuena@gmail.com>
2021-06-07 16:47:26 -0300

    Driver 2.0 agregado con clases

--- scripts/panda_dir.py
+++ scripts/panda_dir.py
@@ -1,26 +1,47 @@
 import rospy
 import time
-import panda_driver_motors
+import panda_driver_motors as pdm
 from motors_panda.msg import Motors
+
+motor_right_front = pdm.motor(pdm.right_front_EN,pdm.right_front_IN_FW,pdm.right_front_IN_BW)
+motor_right_back  = pdm.motor(pdm.right_back_EN,pdm.right_back_IN_FW,pdm.right_back_IN_BW)
+motor_left_front  = pdm.motor(pdm.left_front_EN,pdm.left_front_IN_FW,pdm.left_front_IN_BW)
+motor_left_back   = pdm.motor(pdm.left_back_EN,pdm.left_back_IN_FW,pdm.left_back_IN_BW)
 
 def callback(data):
     rospy.loginfo("I heard LEFT: %f", data.data_l)
     rospy.loginfo("I heard RIGHT: %f", data.data_r)
     if (data.data_l == 0.0 and data.data_r == 0.0):
-        panda_driver_motors.stop()
+        motor_right_front.stop()
+        motor_right_back.stop()
+        motor_left_front.stop()
+        motor_left_back.stop()
     else:
-        panda_driver_motors.move_dir_FW (data.data_l,data.data_r)
+        motor_right_front.move_motor(data.data_r)                                                                   
+        motor_right_back.move_motor(data.data_r)                                                                    
+        motor_left_front.move_motor(data.data_l)                                                                    
+        motor_left_back.move_motor(data.data_l)
     #time.sleep(1)
     #panda_driver_motors.stop()
     #time.sleep(1)
 
 def listener():
-
+    motor_right_front.enable()
+    motor_right_back.enable()
+    motor_left_front.enable()
+    motor_left_back.enable()
+    
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("chatter", Motors, callback)
+    rospy.Subscriber("tracks_duty_cycle", Motors, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    try:
+        listener()
+    except KeyboardInterrupt:
+        motor_right_front.shutdown()                                                                        
+        motor_right_back.shutdown()                                                                         
+        motor_left_front.shutdown()                                                                         
+        motor_left_back.shutdown()
--- scripts/panda_driver_motors.py
+++ scripts/panda_driver_motors.py
@@ -44,20 +44,36 @@ MOTORS = (right_front_EN, right_back_EN, left_front_EN, left_back_EN)
 NORTH = ((right_front_IN_FW, PWM_DUTY),(right_back_IN_FW, PWM_DUTY),
          (left_front_IN_FW, PWM_DUTY),(left_back_IN_FW,PWM_DUTY))
 
-def enable_motor (enable, dir, duty):
+def enable_motor (enable):
     print("Enable motors")
     pca9685.set_PWM(enable,100)
+
+def disable_motor (enable):
+    print("Disable motor")
+    pca9685.set_PWM(enable,0)
+
+def stop_motor (dir):
+    print("Stopping Motor")
+    pca9685.set_PWM(dir,0)
+
+def move_motor (dir,duty):
+    print ("Moving Motor")
     pca9685.set_PWM(dir,duty)
 
-def disable_motor (enable, dir):
-    print("Disable motors")
-    pca9685.set_PWM(enable,0)
-    pca9685.set_PWM(dir,0)
+def disable_motor_and_stop (enable, dir):
+    print("Disable motor and stop")
+    disable_motor(enable)
+    stop_motor (dir)
+
+def enable_motor_and_move (enable, dir, duty):
+    print("Enable motor and move")
+    enable_motor (motor)
+    move_motor(dir,duty)
 
 def move_motors (motors, direction):
     for m, d in zip(motors, direction):
         print(f'Move motors: {m},{d[0]},{d[1]}')
-        enable_motor(m,d[0],d[1])
+        enable_motor_and_move(m,d[0],d[1])
 
 def move_dir_FW (frac_izq, frac_der):
     duty_izq = int(PWM_DUTY*frac_izq)
@@ -81,10 +97,77 @@ def stop():
         pca9685.set_PWM(left_back_IN_BW,0)
         pca9685.set_PWM(left_back_IN_FW,0)
 
-# if __name__ == "__main__":
-#     stop()
-#     time.sleep(1)
-#     move_dir_FW (0.9,0.4)
-#     time.sleep(1)
-#     stop()
-#     time.sleep(1)
+STP  = 0
+FRW  = 1
+BKW  = -1
+
+class motor():
+    motor_HW_EN = 3
+    motor_HW_FW = 4
+    motor_HW_BW = 11
+
+    curr_mov = FRW
+
+    def __init__ (self, EN_pin, FW_pin, BW_pin):
+        self.motor_HW_EN = EN_pin
+        print("Enable en el pin: ", self.motor_HW_EN)
+        self.motor_HW_FW = FW_pin
+        print("Forward en el pin: ", self.motor_HW_FW)
+        self.motor_HW_BW = BW_pin
+        print("Backward en el pin: ",self.motor_HW_BW) 
+        self.curr_mov = FRW
+
+    def enable(self):
+        enable_motor(self.motor_HW_EN)
+
+    def disable(self):
+        disable_motor(self.motor_HW_EN)
+
+    def stop(self):
+        stop_motor(self.motor_HW_FW)
+        stop_motor(self.motor_HW_BW)
+        curr_mov = STP
+
+    def move_motor(self, frac):
+        local_duty = abs(int(frac*PWM_DUTY))
+        print ("Local duty set to: ",local_duty)
+        print ("Frac is set to: ",frac)
+        if frac*self.curr_mov < 0.0:
+            print ("New direction is note the previous one, STOP")
+            print ("Prod of frac*self.curr is: ",frac*self.curr_mov) 
+            self.stop()
+
+        if frac > 0.0:
+            print ("New direction is FW")
+            move_motor(self.motor_HW_FW,local_duty)
+            self.curr_mov = FRW
+
+        if frac < 0.0:
+            print ("New direction is BW")
+            move_motor(self.motor_HW_BW,local_duty)
+            self.curr_mov = BKW
+
+        if frac == 0.0:
+            self.stop()
+            self.curr_mov = STP
+
+    def shutdown(self):
+        self.stop()
+        self.disable()
+
+if __name__ == "__main__":
+    motor_right_front = motor(right_front_EN,right_front_IN_FW,right_front_IN_BW)
+    stop()
+    time.sleep(1)
+    motor_right_front.enable()
+    motor_right_front.move_motor(0.5)
+    time.sleep(1)
+    motor_right_front.move_motor(-0.5)
+    time.sleep(1)
+    motor_right_front.move_motor(0.5)
+    time.sleep(1)
+    motor_right_front.move_motor(-0.5)
+    time.sleep(1)
+    motor_right_front.stop()
+    motor_right_front.shutdown()
+    stop()
