e1fdeaf64e2eb4fd39e201f154480857b8209501
Guillermo Muena <guillermuena@gmail.com>
2022-02-02 17:31:41 -0300

    Agregados comentarios en PCA9685 y en panda_driver_motors (parcial)

--- scripts/panda_driver_motors.py
+++ scripts/panda_driver_motors.py
@@ -1,11 +1,43 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------#
+# File: panda_driver_motors.py                                            #
+# Author: FIE Laboratorio de Robótica Autónoma                            #
+# Developers:                                                             #
+#       Maceira Coni, Carlos Alberto <cmaceira@fie.undef.edu.ar>          #
+#       Muena, Guillermo Ariel       <gmuena@fie.undef.edu.ar>            #
+# Brief:                                                                  #
+#       Archivo de python que contiene la clase necesaria para manejar    #
+#       los motores del robot R2.                                         #
+# Details:                                                                #
+#       Este archivo contiene una clase principal llamada 'motor' que     #
+#       encapsula todos los métodos que se juzgaron necesarios para mane- #
+#       jar un motor a través de un controlador PWM (en este caso un canal#
+#       del microprocesador PCA9685, que tiene una clase propia desarro-  #
+#       llada en python), utilizando como interfaz de potencia un puente H#
+#       La clase también aprovecha las salidas PWM no sólo como salidas   #
+#       digitales para controlar la velocidad del motor, si no también co-#
+#       mo salidas lógicas para controlar la habiltación de las ramas del #
+#       puente H (esto se debe a que le puente H utilizado *HW-095, posee #
+#       entradas lógicas para seleccionar la rama que se verá afectada    #
+#       por el PWM, deshabilitando la otra rama. Como no se quieren agre- #
+#       gar elementos a la electrónica, se utilizan dos salidas PWM como  #
+#       salidas de habilitación, aplicando duties del 100%).              #
+# ------------------------------------------------------------------------#
+
 import PCA9685
 import time
 
+# Init of the I2C PWM controller
 pca9685 = PCA9685.PCA9685()
 
+# Setting the PWM Duty Cycle for all the motors
 PWM_DUTY = 80
+# Setting the PWM Frecuency por all the motors
 PWM_FREQ = 500
 
+# This are the PWM channels used in the current implementation
+# TODO: Pasar todo esto a un archivo de configuración externo.
 right_front_EN = 0
 right_front_IN_BW = 1
 right_front_IN_FW = 2
@@ -21,6 +53,7 @@ left_back_IN_FW = 9
 right_back_EN = 12
 right_back_IN_BW = 13
 right_back_IN_FW = 14
+
 
 pca9685.set_freq(PWM_FREQ)
 pca9685.set_PWM(right_front_EN,0)
